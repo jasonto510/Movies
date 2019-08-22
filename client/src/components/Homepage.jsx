8b699d2c57bf17f6a6ea9342ba275616846778b1
jasonto <jasto@ucdavis.edu>
2019-08-22 13:25:34 -0700

    creating layout to get react running

---
+++ client/src/components/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return(
+      <div>
+        Let's Make A Movie list
+      </div>
+    ) 
+  }
+}
+
+export default HomePage;
---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomePage from './components/Homepage.jsx';
+
+ReactDOM.render(<HomePage/>, document.getElementById('app'));
