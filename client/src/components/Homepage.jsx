88330a26cee4656bb5f863aa2e175e84385985a8
jasonto <jasto@ucdavis.edu>
2019-08-26 22:05:04 -0700

    creating api url endpoint to dynamically render trending moives

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,28 +1,60 @@
 import React from 'react';
 import keys from './apikeys.jsx';
+import $ from 'jquery';
+import TrendingMovies from './TrendingMovies.jsx';
 
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {};
+    this.state = {
+      foundTrending : false,
+      trendingMovies : []
+    };
     this.findMovies = this.findMovies.bind(this);
     this.searchMovies = this.searchMovies.bind(this);
   }
 
+  componentDidMount() {
+    this.getTrendingMovies();
+  }
+
+  getTrendingMovies() {
+    let url = 'https://api.themoviedb.org/3/trending/all/day?api_key=' + keys.movies;
+    $.get(url, (query) => {
+      var titles = query.results;
+      var movies = [];
+      for (var i = 0; i < titles.length; i++) {
+        if (titles[i].original_title) {
+          movies.push(titles[i]);
+        }
+      }
+      this.setState ({
+        foundTrending : true,
+        trendingMovies : movies
+      })
+    })
+  }
+
+
   findMovies(event){
     event.preventDefault();
-    console.log('thsi was clicked')
   }
 
   searchMovies(event) {
     event.preventDefault();
-    console.log(keys.movies)
   }
   render() {
     return(
       <div>
-        Let's Find Some Movies To Watch!
+        <div> 
+          Here are some popular movies!
+          {this.state.foundTrending ? 
+            <div>
+              <TrendingMovies trending={this.state.trendingMovies}/>
+            </div> : null
+          }
+        </div>
         <div>
             <button onClick ={this.findMovies}> Let's Find a Movie To Watch </button> 
         </div>
