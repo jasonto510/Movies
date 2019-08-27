969120638dad7c002a76c961124dbe123a24993d
jasonto <jasto@ucdavis.edu>
2019-08-26 21:21:35 -0700

    created a button to search for movies and created an API key for movies

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import keys from './apikeys.jsx';
+
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {};
+    this.findMovies = this.findMovies.bind(this);
+    this.searchMovies = this.searchMovies.bind(this);
+  }
+
+  findMovies(event){
+    event.preventDefault();
+    console.log('thsi was clicked')
+  }
+
+  searchMovies(event) {
+    event.preventDefault();
+    console.log(keys.movies)
   }
   render() {
     return(
       <div>
-        Let's Make A Movie list
+        Let's Find Some Movies To Watch!
+        <div>
+            <button onClick ={this.findMovies}> Let's Find a Movie To Watch </button> 
+        </div>
+        <form>
+          <label>
+            Let's Find a movie:
+            <input type='text' name='name'/>
+            <button onClick={this.searchMovies}> Enter </button> 
+          </label>
+        </form> 
       </div>
     ) 
   }
